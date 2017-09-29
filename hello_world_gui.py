ab16853aa42b5dec09470d54bd67384e4f3400ce
Scarlett <scarlett.gao@ocsbstudent.ca>
2017-09-29 18:38:04 -0400

    Init

---
+++ hello_world_gui.py
@@ -0,0 +1,9 @@
+# Created by: Scarlett Gao
+# Created on: Sep 2017
+# Created for: ICS3U
+# This program is the Hello, World program, but with a GUI
+
+import ui
+
+view = ui.load_view()
+view.present('sheet')
